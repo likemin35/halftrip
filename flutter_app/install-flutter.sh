d9d39c91867fca2495ca677bf71ff218282747b4
Johamin <gk0042@naver.com>
2026-05-07 01:57:31 +0900

    Pass Vercel env vars into Flutter web build

--- flutter_app/install-flutter.sh
+++ flutter_app/install-flutter.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
-# Flutter가 없으면 다운로드
+
 if [ ! -d "flutter" ]; then
   git clone https://github.com/flutter/flutter.git -b stable
 fi
 
-# 환경 변수 추가
-export PATH="$PATH:`pwd`/flutter/bin"
+export PATH="$PATH:$(pwd)/flutter/bin"
 
-# Flutter 설정 및 빌드
+API_BASE_URL_VALUE="${API_BASE_URL:-https://halftrip-springboot.onrender.com/api}"
+FASTAPI_BASE_URL_VALUE="${FASTAPI_BASE_URL:-https://halftrip-fastapi.onrender.com}"
+USE_MOCK_LOGIN_VALUE="${USE_MOCK_LOGIN:-true}"
+USE_MOCK_API_VALUE="${USE_MOCK_API:-false}"
+MAP_PROVIDER_VALUE="${MAP_PROVIDER:-mock}"
+KAKAO_MAP_APP_KEY_VALUE="${KAKAO_MAP_APP_KEY:-}"
+
 flutter config --enable-web
-flutter build web --release
+flutter build web --release \
+  --dart-define=API_BASE_URL="$API_BASE_URL_VALUE" \
+  --dart-define=FASTAPI_BASE_URL="$FASTAPI_BASE_URL_VALUE" \
+  --dart-define=USE_MOCK_LOGIN="$USE_MOCK_LOGIN_VALUE" \
+  --dart-define=USE_MOCK_API="$USE_MOCK_API_VALUE" \
+  --dart-define=MAP_PROVIDER="$MAP_PROVIDER_VALUE" \
+  --dart-define=KAKAO_MAP_APP_KEY="$KAKAO_MAP_APP_KEY_VALUE"
